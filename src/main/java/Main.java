47f09f8fdbea38fe9bacf4d7d4515e07ee22916d
isurukavinda99 <isurukavinda999@gmail.com>
2022-09-26 10:50:23 +0530

    problem 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -68,6 +68,22 @@ public class Main {
 
     }
 
+    // problem 3 ---------------------------
+    public List<Integer> calculateFibonacci(){
+
+        // initialize first Fibonacci number as 0 and second Fibonacci number as 1
+        List<Integer> fibonacciNumbers = new ArrayList<>(Arrays.asList(0,1));
+
+        int numberOfIterations = 100;
+
+        for(int itr = 2 ; itr < numberOfIterations ; itr++){
+            fibonacciNumbers.add(fibonacciNumbers.get(itr - 1) + fibonacciNumbers.get(itr - 2));
+        }
+
+        return fibonacciNumbers;
+
+    }
+
 }
 
 
